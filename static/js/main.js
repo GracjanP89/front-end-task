aa87697c21015cca85cffff3448bc040812d4589
Gracjan Plizga <gratius1989@wp.pl>
2018-10-13 22:53:40 +0200

    move main data to external file , add additional changes to filter feature

--- static/js/main.js
+++ static/js/main.js
@@ -1,72 +1,3 @@
-var data = [
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/9780596517748/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/9780596517748/cat.gif"
-    },
-    "title": "JavaScript: The Good Parts",
-    "author": "Douglas Crockford",
-    "releaseDate": "12/2008",
-    "pages": 172,
-    "link": "http://shop.oreilly.com/product/9780596517748.do"
-  },
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/9780596000486/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/9780596000486/cat.gif"
-    },
-    "title": "JavaScript: The Definitive Guide",
-    "author": "David Flanagan",
-    "releaseDate": "11/2001",
-    "pages": 936,
-    "link": "http://shop.oreilly.com/product/9780596000486.do"
-  },
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/0636920025832/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/0636920025832/cat.gif"
-    },
-    "title": "Learning JavaScript Design Patterns",
-    "author": "Addy Osmani",
-    "releaseDate": "08/2012",
-    "pages": 254,
-    "link": "http://shop.oreilly.com/product/0636920025832.do"
-  },
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/0636920027713/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/0636920027713/cat.gif"
-    },
-    "title": "JavaScript Enlightenment",
-    "author": "Cody Lindley",
-    "releaseDate": "12/2012",
-    "pages": 166,
-    "link": "http://shop.oreilly.com/product/0636920027713.do"
-  },
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/0636920033141/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/0636920033141/cat.gif"
-    },
-    "title": "Programming JavaScript Applications",
-    "author": "Eric Elliott",
-    "releaseDate": "07/2014",
-    "pages": 254,
-    "link": "http://shop.oreilly.com/product/0636920033141.do"
-  },
-  {
-    "cover": {
-      "large": "https://covers.oreillystatic.com/images/0636920047124/lrg.jpg",
-      "small": "https://covers.oreillystatic.com/images/0636920047124/cat.gif"
-    },
-    "title": "Practical Modern JavaScript",
-    "author": "Nicolas Bevacqua",
-    "releaseDate": "07/2017",
-    "pages": 334,
-    "link": "http://shop.oreilly.com/product/0636920047124.do"
-  }
-];
-
 // -------------------------
 
 
@@ -122,21 +53,24 @@ Product.prototype.sortProducts = function(typeSort){
   switch (typeSort) {
     case "page":
         data = this.sortPage(data);
-        this.createItems(data);
+        var dataRender = data;
+        this.createItems(this.pageFilter(dataRender));
         break;
     case "data":
         data = this.sortData(data);
-        this.createItems(data);
+        var dataRender = data;
+        this.createItems(this.pageFilter(dataRender));
         break;
     case "subname":
         data = this.subName(data);
-        this.createItems(data);
+        var dataRender = data;
+        this.createItems(this.pageFilter(dataRender));
         break;
       }
 }
 
 Product.prototype.sortPage = function(data){
-  var data = this.pageFilter(data);
+  var dataFilter = this.pageFilter(data);
   function compare(a,b) {
     if (a.pages < b.pages)
       return -1;
@@ -150,7 +84,7 @@ Product.prototype.sortPage = function(data){
 }
 
 Product.prototype.sortData = function(data){
-  var data = this.pageFilter(data);
+  var dataFilter = this.pageFilter(data);
   function compare(a,b) {
     var a = a.releaseDate.split("/");
     var b = b.releaseDate.split("/");
@@ -169,7 +103,7 @@ Product.prototype.sortData = function(data){
 }
 
 Product.prototype.subName = function(data){
-  var data = this.pageFilter(data);
+  var dataFilter = this.pageFilter(data);
 
   function compare(a,b) {
     var a = a.author.split(" ");
